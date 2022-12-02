fe0a65823e91f6eb267fab7ebbb9e79a03591d97
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-12-02 12:52:45 +0900

    add mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configValidationSchema } from './config.schema';
@@ -9,6 +10,13 @@ import { configValidationSchema } from './config.schema';
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: configValidationSchema,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
