6a4a6455979d795aa661543a9e73915ba2f86d5f
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-12-03 14:39:46 +0900

    enable global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -13,6 +13,8 @@ async function bootstrap() {
   const config = new DocumentBuilder().setTitle('Invoice API').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const logger = new Logger('Main', { timestamp: true });
 
